a227359538a35370c377f86c975f8b651f516ceb
niccokuc <nerminkuc74@gmail.com>
2016-09-08 07:18:31 +0400

    creating a function

--- IP_Scan.py
+++ IP_Scan.py
@@ -22,3 +22,23 @@ with closing(urllib.request.urlopen(url)) as response:
         print("zip code : " + location_zip)
         print("ip : "+location_ip)
         print(location)
+
+def gioIP(self):
+    url = 'http://freegeoip.net/json/' + Url_Input
+    with closing(urllib.request.urlopen(url)) as response:
+        for line in response:
+            # print(line.decode('utf-8'))
+            location = json.loads(line.decode('utf-8'))
+            location_ip = location['ip']
+            location_city = location['city']
+            location_state = location['region_name']
+            location_country = location['country_name']
+            location_zip = location['zip_code']
+            print("country : " + location_country)
+            print("city : " + location_city)
+            print("state : " + location_state)
+            print("zip code : " + location_zip)
+            print("ip : "+location_ip)
+            print(location)
+
+gioIP("www.cnn.com")
